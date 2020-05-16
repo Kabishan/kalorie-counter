78182db966a22f32714f1cc71b316f45cc4022cf
Kabishan <suvendrk@mcmaster.ca>
2020-05-16 10:15:59 -0400

    Added item ctrlr, ui ctrlr, app ctrlr and was able to update the page with hardcoded data; progress was halted due to power outage yesterday

---
+++ app.js
@@ -0,0 +1,61 @@
+const ItemCtrl = (function () {
+  const Item = function (id, name, calories) {
+    this.id = id;
+    this.name = name;
+    this.calories = calories;
+  };
+
+  const data = {
+    items: [
+      { id: 0, name: 'Steak Dinner', calories: 1200 },
+      { id: 1, name: 'Cookie', calories: 400 },
+      { id: 2, name: 'Eggs', calories: 300 },
+    ],
+    currentItem: null,
+    totalCalories: 0,
+  };
+
+  return {
+    getItems: function () {
+      return data.items;
+    },
+    logData: function () {
+      return data;
+    },
+  };
+})();
+
+const UICtrl = (function () {
+  const UIElements = {
+    itemList: 'item-list',
+  };
+  return {
+    populate: function (items) {
+      let output = '';
+
+      items.forEach(function (item) {
+        output += `
+        <li class="collection-item" id="item-${item.id}">
+            <strong>${item.name}: </strong> 
+            <em>${item.calories} Calories</em>
+            <a href="#" class="secondary-content">
+                <i class=" edit-item fa fa-pencil"></i>
+            </a>
+        </li>`;
+      });
+
+      document.getElementById(UIElements.itemList).innerHTML = output;
+    },
+  };
+})();
+
+const App = (function (ItemCtrl, UICtrl) {
+  return {
+    init: function () {
+      const items = ItemCtrl.getItems();
+      UICtrl.populate(items);
+    },
+  };
+})(ItemCtrl, UICtrl);
+
+App.init();
